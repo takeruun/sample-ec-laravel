47adcd6a7bfe38fca8435dfd00afec3af5e9d45a
takeru.hashimoto <hstake25@gmail.com>
2022-02-23 23:20:19 +0900

    resource controller

--- laravel/resources/views/layouts/admin-navigation.blade.php
+++ laravel/resources/views/layouts/admin-navigation.blade.php
@@ -12,8 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('admin.owners.index')" :active="request()->routeIs('admin.owners.index')">
+                        オーナー管理
                     </x-nav-link>
                 </div>
             </div>
--- laravel/routes/admin.php
+++ laravel/routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
   return view('admin.dashboard');
 })->middleware(['auth:admin'])->name('dashboard');
+
+Route::resource('owners', OwnersController::class)
+  ->middleware('auth:admin');
 
 
 Route::middleware('guest')->group(function () {
