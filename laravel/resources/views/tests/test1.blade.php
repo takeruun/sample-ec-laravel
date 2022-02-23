1d519f22d9b76565261bdb23e60378e82424a628
takeru.hashimoto <hstake25@gmail.com>
2022-02-23 12:23:06 +0900

    属性バック

--- laravel/resources/views/tests/test1.blade.php
+++ laravel/resources/views/tests/test1.blade.php
@@ -5,4 +5,5 @@
   テスト１
   <x-tests.card title="タイトル" content="メッセージ" :message="$message"></x-tests.card>
   <x-tests.card title="タイトル2"></x-tests.card>
+  <x-tests.card title="タイトル3" class="bg-red-300"></x-tests.card>
 </x-tests.app>
