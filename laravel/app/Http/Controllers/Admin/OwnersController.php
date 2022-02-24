8215a427bea8206a573e2695f7678719c566c285
takeru.hashimoto <hstake25@gmail.com>
2022-02-25 00:15:03 +0900

    テーブル操作

--- laravel/app/Http/Controllers/Admin/OwnersController.php
+++ laravel/app/Http/Controllers/Admin/OwnersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Owner; // Eloguent エロクエント
+use Illuminate\Support\Facades\DB; // QueryBuilder クエリビルダー
 
 class OwnersController extends Controller
 {
@@ -18,7 +20,15 @@ class OwnersController extends Controller
      */
     public function index()
     {
-        dd('オーナー一覧');
+        $e_all = Owner::all();
+        $q_get = DB::table('owners')->select('name')->get();
+        $q_first = DB::table('owners')->select('name')->first();
+
+        $c_test = collect([
+            'name' => 'てすと'
+        ]);
+
+        dd($e_all, $q_get, $q_first, $c_test);
     }
 
     /**
